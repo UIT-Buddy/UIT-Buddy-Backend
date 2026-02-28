689f4e5562e8f8e3ac6fe904f61bfce6e6e3fb1d
Minh dep trai <phandinhminh48@gmail.com>
2026-02-28 20:57:50 +0700

    feat(ci): add CI workflow for building, testing, and Docker image deployment
    fix: update .gitignore to exclude all JSON files
    
    feat(docker): mount Firebase admin SDK JSON file in Docker container
    
    fix: change main method visibility to static in BuddyApplication

--- src/main/java/com/uit/buddy/BuddyApplication.java
+++ src/main/java/com/uit/buddy/BuddyApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BuddyApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(BuddyApplication.class, args);
     }
 }
