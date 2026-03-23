9c5ebc62cd7eb26c0b5fa86112bbb5cf29727eff
Minh Phan Dinh <phandinhminh48@gmail.com>
2026-03-23 14:01:19 +0700

    add no pass

--- infrastructure/terraform/user_data.sh
+++ infrastructure/terraform/user_data.sh
@@ -2,6 +2,7 @@
 apt-get update -y
 apt-get install -y podman podman-compose
 useradd -m github-runner
+echo "github-runner ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/github-runner
 su - github-runner -c "mkdir actions-runner && cd actions-runner && curl -o actions-runner-linux-x64-2.311.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.311.0/actions-runner-linux-x64-2.311.0.tar.gz"
 su - github-runner -c "cd actions-runner && tar xzf ./actions-runner-linux-x64-2.311.0.tar.gz"
 
