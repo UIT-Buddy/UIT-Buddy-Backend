8d8244f76f4c1394a14ac89191ac1c364b367cd6
Minh Phan Dinh <phandinhminh48@gmail.com>
2026-03-23 14:14:59 +0700

    add make package installation to user_data.sh

--- infrastructure/terraform/user_data.sh
+++ infrastructure/terraform/user_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update -y
-apt-get install -y podman podman-compose
+apt-get install -y podman podman-compose make
 useradd -m github-runner
 echo "github-runner ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/github-runner
 su - github-runner -c "mkdir actions-runner && cd actions-runner && curl -o actions-runner-linux-x64-2.311.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.311.0/actions-runner-linux-x64-2.311.0.tar.gz"
