4563c4bbfaa1d3bc4082e27dec232dbdc1113248
Mirai3103 <huuhoag1412@gmail.com>
2026-03-28 12:48:13 +0700

    sort import order

--- src/main/java/com/uit/buddy/config/S3Config.java
+++ src/main/java/com/uit/buddy/config/S3Config.java
@@ -1,13 +1,13 @@
 package com.uit.buddy.config;
 
 import java.net.URI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.S3ClientBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class S3Config {
