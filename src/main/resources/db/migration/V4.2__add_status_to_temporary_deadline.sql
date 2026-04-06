acdeda860e6bcdb07e0808d70d161318653babc1
Tientryhard <tienvm886@gmail.com>
2026-04-06 08:46:03 +0700

    fix not null column

--- src/main/resources/db/migration/V4.2__add_status_to_temporary_deadline.sql
+++ src/main/resources/db/migration/V4.2__add_status_to_temporary_deadline.sql
@@ -1,4 +1,4 @@
 ALTER TABLE temporary_deadline
-    ADD COLUMN status VARCHAR(20) NOT NULL;
+    ADD COLUMN status VARCHAR(20);
 ALTER TABLE temporary_deadline
-    ADD COLUMN semester_code VARCHAR(20) NOT NULL;
+    ADD COLUMN semester_code VARCHAR(20);
