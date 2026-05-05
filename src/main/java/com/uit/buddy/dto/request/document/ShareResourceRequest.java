234bd72812e033b630b7c82269e446fe4a4c6ef3
TuanTran <trananhtuan2005dh@gmail.com>
2026-05-05 22:57:18 +0700

    Feat/update share roles (#110)
    * feat(document): add roles
    
    * delete redundant dto

--- src/main/java/com/uit/buddy/dto/request/document/ShareResourceRequest.java
+++ src/main/java/com/uit/buddy/dto/request/document/ShareResourceRequest.java
@@ -1,5 +1,6 @@
 package com.uit.buddy.dto.request.document;
 
+import com.uit.buddy.enums.AccessRole;
 import com.uit.buddy.enums.DocumentResourceType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -7,5 +8,6 @@ import java.util.UUID;
 
 public record ShareResourceRequest(@NotNull(message = "Resource type is required") DocumentResourceType resourceType,
         @NotNull(message = "Resource ID is required") UUID resourceId,
-        @NotBlank(message = "Target MSSV is required") String targetMssv) {
+        @NotBlank(message = "Target MSSV is required") String targetMssv,
+        @NotNull(message = "Access role is required") AccessRole accessRole) {
 }
